c5e36344135bde11db0e0e9cd4bc46e0511bf5dd
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-04 12:47:59 +0100

    Reorganize directory structure

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -1,17 +1,6 @@
-#! /usr/bin/env sh
+#!/usr/bin/env bash
 
-# Exit in case of error
 set -e
+set -x
 
-DOMAIN=backend \
-SMTP_HOST="" \
-INSTALL_DEV=true \
-docker-compose \
--f docker-compose.yml \
-config > docker-stack.yml
-
-docker-compose -f docker-stack.yml build
-docker-compose -f docker-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
-docker-compose -f docker-stack.yml up -d
-docker-compose -f docker-stack.yml exec -T backend bash /app/tests-start.sh "$@"
-docker-compose -f docker-stack.yml down -v --remove-orphans
+pytest --cov=app --cov-report=term-missing app/tests "${@}"
