2310604f9625c13d3660388369e5151cf1dd249a
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-06 14:36:40 +0100

    Add Car card

--- frontend/src/views/cars/CarsListView/CarCard.js
+++ frontend/src/views/cars/CarsListView/CarCard.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import {
-  Box,
-  Card,
-  CardContent,
-  Divider,
-  Grid,
-  makeStyles,
-  Typography
-} from '@material-ui/core';
-import Image from 'material-ui-image';
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { Box, Card, CardContent, Divider, Grid, makeStyles, Paper, Typography } from "@material-ui/core";
+import LocalGasStationIcon from "@material-ui/icons/LocalGasStation";
+import SettingsIcon from "@material-ui/icons/Settings";
+import PersonIcon from '@material-ui/icons/Person';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import AcUnitIcon from '@material-ui/icons/AcUnit';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column'
+    display: "flex",
+    flexDirection: "column"
   },
   statsItem: {
-    alignItems: 'center',
-    display: 'flex'
+    alignItems: "center",
+    display: "flex"
   },
   statsIcon: {
     marginRight: theme.spacing(1)
@@ -37,7 +33,9 @@ const CarCard = ({ className, product, ...rest }) => {
     <Card className={clsx(classes.root, className)} {...rest}>
       <CardContent>
         <Box display="flex" justifyContent="center" mb={3}>
-          <Image src={product.media} />
+          <Paper variant="outlined">
+            <img src={product.media} alt={product.modelName} width="500px" height="250px" />
+          </Paper>
         </Box>
         <Typography align="left" color="textPrimary" gutterBottom variant="h4">
           {product.modelName}
@@ -50,8 +48,31 @@ const CarCard = ({ className, product, ...rest }) => {
       <Divider />
       <Box p={2}>
         <Grid container justify="space-between" spacing={2}>
-          <Grid className={classes.statsItem} item>
-            test
+          <Grid className={classes.statsItem} item lg={3} md={3}>
+            <LocalGasStationIcon />
+            <Typography color="textPrimary" variant="h7">{product.fuelType}</Typography>
+          </Grid>
+          <Grid className={classes.statsItem} item lg={3} md={3}>
+            <SettingsIcon />
+            <Typography color="textPrimary" variant="h7">{product.gearboxType}</Typography>
+          </Grid>
+          <Grid className={classes.statsItem} item lg={4} md={4}>
+            <AttachMoneyIcon />
+            <Typography color="textPrimary" variant="h5">{product.pricePerDay} zł / dzień</Typography>
+          </Grid>
+        </Grid>
+        <Grid container justify="space-between" spacing={2}>
+          <Grid className={classes.statsItem} item lg={3} md={3}>
+            <AcUnitIcon />
+            <Typography color="textPrimary" variant="h7">{product.acType}</Typography>
+          </Grid>
+          <Grid className={classes.statsItem} item lg={3} md={3}>
+            <PersonIcon />
+            <Typography color="textPrimary" variant="h7">{product.numberOfPassengers}</Typography>
+          </Grid>
+          <Grid className={classes.statsItem} item lg={4} md={4}>
+            <AttachMoneyIcon />
+            <Typography color="textPrimary" variant="h6">{product.depositAmount} zł kaucji</Typography>
           </Grid>
         </Grid>
       </Box>
--- frontend/src/views/cars/CarsListView/data.js
+++ frontend/src/views/cars/CarsListView/data.js
@@ -1,39 +1,70 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 export default [
   {
     id: uuid(),
-    modelName: 'FIAT 500',
-    media: '/static/images/cars/car_1.png'
+    modelName: "FIAT 500",
+    media: "/static/images/cars/car_1.png",
+    fuelType: "PETROL",
+    gearboxType: "AUTO",
+    acType: "AUTO",
+    numberOfPassengers: 4,
+    pricePerDay: 300.00,
+    depositAmount: 1000.00
   },
   {
     id: uuid(),
-    modelName:
-      'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
-    media: '/static/images/cars/car_2.png'
+    modelName: "HYUNDAI I20",
+    media: "/static/images/cars/car_2.png",
+    fuelType: "PETROL",
+    gearboxType: "AUTO",
+    acType: "AUTO",
+    numberOfPassengers: 4,
+    pricePerDay: 300.00,
+    depositAmount: 1000.00
   },
   {
     id: uuid(),
-    modelName:
-      'Slack is a cloud-based set of team collaboration tools and services, founded by Stewart Butterfield.',
-    media: '/static/images/cars/car_3.png'
+    modelName: "VOLKSWAGEN POLO",
+    media: "/static/images/cars/car_3.png",
+    fuelType: "PETROL",
+    gearboxType: "AUTO",
+    acType: "AUTO",
+    numberOfPassengers: 4,
+    pricePerDay: 300.00,
+    depositAmount: 1000.00
   },
   {
     id: uuid(),
-    modelName:
-      'Lyft is an on-demand transportation company based in San Francisco, California.',
-    media: '/static/images/cars/car_4.png'
+    modelName: "TOYOTA COROLLA",
+    media: "/static/images/cars/car_4.png",
+    fuelType: "PETROL",
+    gearboxType: "AUTO",
+    acType: "AUTO",
+    numberOfPassengers: 4,
+    pricePerDay: 300.00,
+    depositAmount: 1000.00
   },
   {
     id: uuid(),
-    modelName:
-      'GitHub is a web-based hosting service for version control of code using Git.',
-    media: '/static/images/cars/car_5.png'
+    modelName: "RENAULT MASTER",
+    media: "/static/images/cars/car_5.png",
+    fuelType: "PETROL",
+    gearboxType: "AUTO",
+    acType: "AUTO",
+    numberOfPassengers: 4,
+    pricePerDay: 300.00,
+    depositAmount: 1000.00
   },
   {
     id: uuid(),
-    modelName:
-      'Squarespace provides software as a service for website building and hosting. Headquartered in NYC.',
-    media: '/static/images/cars/car_6.png'
+    modelName: "MERCEDES E220",
+    media: "/static/images/cars/car_6.png",
+    fuelType: "PETROL",
+    gearboxType: "AUTO",
+    acType: "AUTO",
+    numberOfPassengers: 4,
+    pricePerDay: 300.00,
+    depositAmount: 1000.00
   }
 ];
