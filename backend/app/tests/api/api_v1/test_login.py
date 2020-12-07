3335c536e6da3876f6061fd5349b77d286b86fde
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-07 23:32:59 +0100

    Delete redundant test

--- backend/app/tests/api/api_v1/test_login.py
+++ backend/app/tests/api/api_v1/test_login.py
@@ -15,14 +15,3 @@ def test_get_access_token(client: TestClient) -> None:
     assert r.status_code == 200
     assert "access_token" in tokens
     assert tokens["access_token"]
-
-
-def test_use_access_token(
-    client: TestClient, superuser_token_headers: Dict[str, str]
-) -> None:
-    r = client.post(
-        f"{settings.API_V1_STR}/login/test-token", headers=superuser_token_headers,
-    )
-    result = r.json()
-    assert r.status_code == 200
-    assert "email" in result
