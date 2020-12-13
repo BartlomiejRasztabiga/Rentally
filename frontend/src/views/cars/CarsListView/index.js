ccec72ed8ba2ffcca06073a3de0002eeecfeef18
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-13 22:24:30 +0100

    Add simple search bar for cars

--- frontend/src/views/cars/CarsListView/index.js
+++ frontend/src/views/cars/CarsListView/index.js
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Box, Container, Grid, makeStyles } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  InputAdornment,
+  makeStyles,
+  SvgIcon,
+  TextField
+} from "@material-ui/core";
 import Page from "src/components/Page";
-import Toolbar from "./Toolbar";
 import CarCard from "./CarCard";
 import { getCars } from "../../../service/carsService";
+import { Search as SearchIcon } from "react-feather";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,6 +31,7 @@ const useStyles = makeStyles((theme) => ({
 const CarsList = () => {
   const classes = useStyles();
   const [cars, setCars] = useState([]);
+  const [searchPhrase, setSearchPhrase] = useState("");
 
   useEffect(() => {
     getCars().then(cars => {
@@ -30,10 +42,39 @@ const CarsList = () => {
   return (
     <Page className={classes.root}>
       <Container maxWidth={false}>
-        <Toolbar />
+        {/*TODO can extract this earch box to another component and share state? redux or react context?*/}
+        <Box display="flex" justifyContent="flex-end">
+          <Button color="primary" variant="contained">
+            Add car
+          </Button>
+        </Box>
+        <Box mt={3}>
+          <Card>
+            <CardContent>
+              <Box maxWidth={500}>
+                <TextField
+                  fullWidth
+                  value={searchPhrase}
+                  onChange={e => setSearchPhrase(e.target.value)}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SvgIcon fontSize="small" color="action">
+                          <SearchIcon />
+                        </SvgIcon>
+                      </InputAdornment>
+                    )
+                  }}
+                  placeholder="Search car"
+                  variant="outlined"
+                />
+              </Box>
+            </CardContent>
+          </Card>
+        </Box>
         <Box mt={3}>
           <Grid container spacing={3}>
-            {cars.map((car) => (
+            {cars.filter(car => car.model_name.toLowerCase().includes(searchPhrase.toLowerCase())).map((car) => (
               <Grid item key={car.id} lg={4} md={4} xs={12}>
                 <CarCard className={classes.carCard} car={car} />
               </Grid>
