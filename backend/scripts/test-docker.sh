2352db4e12a07c937dc19cd2d1a649d77552bc56
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-14 23:16:20 +0100

    Do not exit when test-docker fails

--- backend/scripts/test-docker.sh
+++ backend/scripts/test-docker.sh
@@ -1,11 +1,8 @@
 #! /usr/bin/env bash
 
-# Exit in case of error
-set -e
-
 docker-compose down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
 
 docker-compose build
 docker-compose up -d
-docker-compose exec -T backend bash ./prestart.sh
-docker-compose exec -T backend bash ./tests-start.sh
+docker-compose exec -T backend bash ./prestart.sh "$@"
+docker-compose exec -T backend bash ./tests-start.sh "$@"
