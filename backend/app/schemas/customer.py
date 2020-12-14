b3cf86a4ca76b62ab6cdfda080e1949396470bf9
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-14 17:43:50 +0100

    Add customer schemas

---
+++ backend/app/schemas/customer.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+# Shared properties
+class CustomerBase(BaseModel):
+    full_name: str
+    address: Optional[str]
+    phone_number: Optional[str]
+
+
+# Properties to receive via API on creation
+class CustomerCreateDto(CustomerBase):
+    pass
+
+
+# Properties to receive via API on update
+_update_optional_fields = CustomerBase.__fields__.keys()
+
+
+# Properties to receive via API on update
+class CustomerUpdateDto(CustomerBase, optional_fields=_update_optional_fields):
+    pass
+
+
+class CustomerInDBBase(CustomerBase):
+    id: Optional[int] = None
+
+    class Config:
+        orm_mode = True
+
+
+# Additional properties to return via API
+class Customer(CustomerInDBBase):
+    pass
+
+
+# Additional properties stored in DB
+class CustomerInDB(CustomerInDBBase):
+    pass
