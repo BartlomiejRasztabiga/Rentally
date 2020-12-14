4f968122e748997dfd9efae22557c234e94d18cc
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-14 21:06:27 +0100

    Add reservation model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,4 @@
 from .car import Car
 from .customer import Customer
+from .reservation import Reservation
 from .user import User
---
+++ backend/app/models/reservation.py
@@ -0,0 +1,24 @@
+import enum
+
+from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer
+
+from app.db.base_class import Base
+
+
+# fmt: off
+class ReservationStatus(enum.Enum):
+    NEW = "NEW"
+    COMPLETED = "COMPLETED"
+    CANCELLED = "CANCELLED"
+
+
+# fmt: on
+
+
+class Reservation(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    car_id = Column(Integer, ForeignKey("car.id"), nullable=False)
+    customer_id = Column(Integer, ForeignKey("customer.id"), nullable=False)
+    start_date = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=False)
+    status = Column(Enum(ReservationStatus), nullable=False)
