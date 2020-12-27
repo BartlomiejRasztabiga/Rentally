952ee90aa0b81ec5f02ee462103df6c265d7a858
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-27 19:49:04 +0100

    Use env variable for api url if exists

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api/v1";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api/v1";
 const ACCESS_TOKEN_URL = API_URL + "/login/access-token";
 const GET_ME_URL = API_URL + "/users/me";
 const CARS_URL = API_URL + "/cars";
