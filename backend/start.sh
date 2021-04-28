90ab3447b4eafb962f7e79e75fb1c5c33ec4e419
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-04-28 22:49:01 +0200

    Add dockerfiles and docker compose config

--- backend/start.sh
+++ backend/start.sh
@@ -3,6 +3,16 @@
 # Print commands and their arguments as they are executed.
 set -x
 
+# Let the DB start
+python ./app/pre_start.py
+
+# Run migrations
+alembic upgrade head
+
+# Create initial data in DB
+python ./app/seed.py
+
+
 HOST=${HOST:-0.0.0.0}
 PORT=${PORT:-8080}
 LOG_LEVEL=${LOG_LEVEL:-info}
